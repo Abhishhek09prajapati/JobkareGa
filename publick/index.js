31858e519278caa614b3f07158fd2daeea067f60
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-07-14 10:10:50 +0530

    df

--- publick/index.js
+++ publick/index.js
@@ -1,3 +1,7 @@
-var a = localStorage.getItem("userID")
+// var a = localStorage.getItem("userID")
+// document.getElementById("k").textContent = a
+document.getElementById("sildebaar").style.display = "none"
 
-document.getElementById("k").textContent  = a
+document.getElementById("claosespan").addEventListener('click', () => {
+    document.getElementById("sildebaar").style.width = "0px"
+})
