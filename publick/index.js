8dd46364ac217afa236ca2ab3cd9df5b0ea62cd1
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-07-14 11:01:42 +0530

    h

--- publick/index.js
+++ publick/index.js
@@ -5,3 +5,9 @@ document.getElementById("sildebaar").style.display = "none"
 document.getElementById("claosespan").addEventListener('click', () => {
     document.getElementById("sildebaar").style.width = "0px"
 })
+
+var number = "6387215755";
+
+document.getElementById("callBtn").addEventListener("click", function () {
+    window.location.href = `tel:${number}`;
+});
