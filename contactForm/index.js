f6b8a70ec3466f9c69cc5f7a28bc4e72697183f7
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-08-14 08:28:29 +0530

    dd

--- contactForm/index.js
+++ contactForm/index.js
@@ -1,6 +1,12 @@
 var loading = document.getElementById("loading")
 loading.style.display = "none" ;
-var msg = "Ham ne Form Fill Kar Diya Hai , Please Provide Me Links and UserID and Password"
+var msg = "Ham ne Form Fill Kar Diya Hai , Please Provide Me Links and UserID and Password" ;
+
+var s = localStorage.getItem("userID")
+
+console.log(s)
+
+
 
 document.getElementById("contactForm").addEventListener("submit", async (e) => {
     e.preventDefault();
